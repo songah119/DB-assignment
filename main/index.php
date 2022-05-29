2dde5c49cf5fd55bb1713b6b6db525fb1edbd2d5
songah119 <skutcm119@gmail.com>
2022-05-29 23:28:01 +0900

    [etc] 폴더 구조 변경

---
+++ main/index.php
@@ -0,0 +1,22 @@
+<?php
+?>
+
+<html>
+    <head>
+        <title>이화 푸드코드 매출 보고서</title>
+        <link rel="stylesheet" href="index.css">
+    </head>
+    <body>
+        <header>
+            <h1>이화 푸드코드 매출 보고서</h1>
+        </header>
+        <main>
+            <article class="report">
+                <div class="router customer" onClick="location.href='http://localhost:8888/report/customer'">고객 유형별 주문 건수와 매출</div>
+                <div class="router time" onClick="location.href='http://localhost:8888/report/time'">시간대별 주문 건수와 매출</div>
+                <div class="router corner" onClick="location.href='http://localhost:8888/report/corner'">코너별 주문 건수와 매출</div>
+            </article>
+        </main>
+
+    </body>
+</html>
